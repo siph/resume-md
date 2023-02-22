fadd1bfec0703943d732934b4489c1bc6ceeb380
siph <dawkins.chris.dev@gmail.com>
2023-02-22 11:23:40 -0700

    nix shell

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Build you resume with markdown";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, nixos-generators, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config = {
+            permittedInsecurePackages = [ "qtwebkit-5.212.0-alpha4" ];
+          };
+        };
+      in rec {
+        devShell = with pkgs;
+          mkShell {
+            buildInputs = [
+              pandoc
+              wkhtmltopdf-bin
+            ];
+          };
+      });
+}
+
