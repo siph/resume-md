c1f90e22d7700157aebd3f1a60907e907d396759
siph <dawkins.chris.dev@gmail.com>
2023-09-24 12:16:42 -0600

    flake check

--- flake.nix
+++ flake.nix
@@ -9,38 +9,55 @@
   outputs = { nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
+
         pkgs = import nixpkgs {
           inherit system;
           config = {
             permittedInsecurePackages = [ "openssl-1.1.1v" ];
           };
         };
+
         buildInputs = with pkgs; [
           pandoc
           wkhtmltopdf-bin
         ];
+
+        buildPhase = ''
+          pandoc resume.md \
+          -t html -f markdown \
+          -c style.css --self-contained \
+          -o resume.html
+
+          wkhtmltopdf --enable-local-file-access \
+          resume.html \
+          resume.pdf
+        '';
+
       in with pkgs; {
+
         packages = {
-          default = stdenv.mkDerivation {
-            inherit buildInputs;
+          default = stdenvNoCC.mkDerivation {
+            inherit buildInputs buildPhase;
             name = "resume_md";
             src = ./.;
-            buildPhase = ''
-              pandoc resume.md \
-              -t html -f markdown \
-              -c style.css --self-contained \
-              -o resume.html
-
-              wkhtmltopdf --enable-local-file-access \
-              resume.html \
-              resume.pdf
-            '';
             installPhase = ''
               mkdir -p $out/resume
               cp resume.* $out/resume/
             '';
           };
         };
+
+        checks = {
+          default = stdenvNoCC.mkDerivation {
+            inherit buildInputs buildPhase;
+            name = "resume-md checks";
+            src = ./.;
+            installPhase = ''
+              mkdir -p $out
+            '';
+          };
+        };
+
         devShell = mkShell {
           inherit buildInputs;
         };
